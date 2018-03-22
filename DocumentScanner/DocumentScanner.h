7f26781f1af00c5941212d88b16e4fb3b7046476
StanDimitroff <standimitroff@gmail.com>
2018-03-22 11:43:27 +0200

    feat: add Carthage support

---
+++ DocumentScanner/DocumentScanner.h
@@ -0,0 +1,19 @@
+//
+//  DocumentScanner.h
+//  DocumentScanner
+//
+//  Created by Stanislav Dimitrov on 22.03.18.
+//  Copyright © 2018 Stanislav Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DocumentScanner.
+FOUNDATION_EXPORT double DocumentScannerVersionNumber;
+
+//! Project version string for DocumentScanner.
+FOUNDATION_EXPORT const unsigned char DocumentScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentScanner/PublicHeader.h>
+
+
